0824b6d5e28e41c3bdf63c1af5795b4572f8abfd
Kyle McCrohan <kmccrohan@zagmail.gonzaga.edu>
2017-12-07 17:13:47 -0800

    Added help section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import Map from "./Map";
 import "./App.css";
+import "./Popup.css";
+import Popup from "react-popup";
+
+const helpText = `Data for this project was taken from the Colorado Avalanche
+Information Center: http://avalanche.state.co.us/accidents/statistics-and-reporting/.
+Source code for this project can be found at: https://github.com/kmccrohan/avalanche-accident-map.
+Collaborators are welcome!`;
 
 class App extends Component {
+  onHelpClick = () => {
+    Popup.alert(helpText, "About");
+  };
+
   render() {
     return (
-      <div className="App">
+      <div className="App" id="App">
+        <Popup />
         <div className="header">
-          <button id="help-button">?</button>
+          <button id="help-button" onClick={this.onHelpClick}>
+            ?
+          </button>
           <h1>❄ Avalanche Accidents in US 2012-2017 ❄</h1>
         </div>
         <div className="map">
